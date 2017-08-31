1c80362489bbe1f5cd6adc99f7ded94fc7dc6b29
HariGN <harikrishnan07@gmail.com>
2017-08-31 16:32:54 +0530

    Update sourcetest.ps1

--- sourcetest.ps1
+++ sourcetest.ps1
@@ -1,10 +1,10 @@
 workflow sourcetest
 {
     Param(
-        $VMName,
+        $VM,
         $ResourceGroup
     )
     $conn = Get-AutomationConnection -Name AzureRunAsConnection
     Add-AzureRMAccount -ServicePrincipal -Tenant $conn.TenantID -ApplicationID $conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
-    Start-AzureRMVM -Name $VMName -ResourceGroupName $ResourceGroup
+    Start-AzureRMVM -Name $VM -ResourceGroupName $ResourceGroup
 }
